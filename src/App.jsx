6e54d474d984f52553e28d7f528b7f775175572d
pandey-sahil <sahilp2593@gmail.com>
2024-10-25 23:30:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LocalStorage from './components/localStorage'
+
+const App = () => {
+  return (
+    <div className='p-5'>
+      <LocalStorage />
+    </div>
+  )
+}
+
+export default App
